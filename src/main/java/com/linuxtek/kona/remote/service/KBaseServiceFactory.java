fa44324bacd807342aaafe4683a498710951b85b
Sharif Alexandre <sharif@fabriq.io>
2016-07-25 14:21:36 -0600

    KBaseServiceFactory: getInstance now returns unique instance for baseUrl and basePackage

--- src/main/java/com/linuxtek/kona/remote/service/KBaseServiceFactory.java
+++ src/main/java/com/linuxtek/kona/remote/service/KBaseServiceFactory.java
@@ -7,7 +7,9 @@ import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.remoting.httpinvoker.HttpComponentsHttpInvokerRequestExecutor;
 import org.springframework.remoting.httpinvoker.HttpInvokerProxyFactoryBean;
 import org.springframework.remoting.httpinvoker.HttpInvokerRequestExecutor;
@@ -16,17 +18,19 @@ import org.springframework.remoting.httpinvoker.HttpInvokerRequestExecutor;
  * 
  */
 public class KBaseServiceFactory {
-	private static Logger logger = Logger.getLogger(KBaseServiceFactory.class);
+	private static Logger logger = LoggerFactory.getLogger(KBaseServiceFactory.class);
     
     private Map<String,Object> serviceCache = null;
     
     private HashMap<String,HttpInvokerProxyFactoryBean> proxyMap = 
             new HashMap<String,HttpInvokerProxyFactoryBean>();
 
-    private static KBaseServiceFactory factory = null;
+    private static Map<String,KBaseServiceFactory> factoryMap = new HashMap<String,KBaseServiceFactory>();
 
     private String basePackage;
     private String baseUrl = null;
+    
+    // ----------------------------------------------------------------------
 
     protected KBaseServiceFactory(String baseUrl) {
         this.baseUrl = baseUrl;
@@ -34,35 +38,47 @@ public class KBaseServiceFactory {
         serviceCache = new HashMap<String,Object>();
     }
     
+    // ----------------------------------------------------------------------
+    
     protected KBaseServiceFactory(String baseUrl, String basePackage) {
         this(baseUrl);
         this.basePackage = basePackage;
     }
+    
+    // ----------------------------------------------------------------------
 
-    public static KBaseServiceFactory getInstance(String baseUrl) {
-        if (factory == null || !factory.getBaseUrl().equals(baseUrl))
+    public static KBaseServiceFactory getInstance(String baseUrl, String basePackage) {
+    	String key = baseUrl+ "|" + basePackage;
+    	
+    	KBaseServiceFactory factory = factoryMap.get(key);
+    	
+        if (factory == null) {
             factory = new KBaseServiceFactory(baseUrl);
-        return (factory);
+            factory.setBasePackage(basePackage);
+            factoryMap.put(key, factory);
+        }
+        
+        return factory;
     }
     
-    public static KBaseServiceFactory getInstance(String baseUrl, String basePackage) {
-        factory = getInstance(baseUrl);
-        factory.setBasePackage(basePackage);
-        return factory;
-        
-    }
+    // ----------------------------------------------------------------------
 
     public void setBasePackage(String basePackage) {
         this.basePackage = basePackage;
     }
     
+    // ----------------------------------------------------------------------
+    
     public String getBaseUrl() {
         return (baseUrl);
     }
     
+    // ----------------------------------------------------------------------
+    
     @SuppressWarnings("rawtypes")
 	protected HttpInvokerProxyFactoryBean getProxy(String serviceName,
             Class serviceInterface) {
+    	
         HttpInvokerProxyFactoryBean proxy = proxyMap.get(serviceName);
         
         if (proxy == null) {
@@ -74,8 +90,11 @@ public class KBaseServiceFactory {
             proxy.afterPropertiesSet();
             proxyMap.put(serviceName, proxy);
         }
+        
         return (proxy);
     }
+    
+    // ----------------------------------------------------------------------
     
     private HttpInvokerRequestExecutor getHttpInvokerRequestExecutor() {
         HttpComponentsHttpInvokerRequestExecutor executor =
@@ -84,21 +103,20 @@ public class KBaseServiceFactory {
         executor.setReadTimeout(0);
         return executor;
     }
+    
+    // ----------------------------------------------------------------------
 
-    protected <I extends KService> I getService(String serviceName,
-            Class<I> serviceInterface) {
+    protected <I extends KService> I getService(String serviceName, Class<I> serviceInterface) {
         I service = null;
 
-        HttpInvokerProxyFactoryBean proxy = 
-            getProxy(serviceName, serviceInterface);
+        HttpInvokerProxyFactoryBean proxy = getProxy(serviceName, serviceInterface);
 
         if (proxy != null) {
             service = serviceInterface.cast(proxy.getObject());
         }
 
         if (service == null) {
-            throw new IllegalStateException(
-                "Unable to instantiate service: " + serviceName);
+            throw new IllegalStateException("Unable to instantiate service: " + serviceName);
         }
 
         logger.debug("fetching service [" + serviceName + "]: " + service);
@@ -106,9 +124,13 @@ public class KBaseServiceFactory {
         return (service);
     }
     
+    // ----------------------------------------------------------------------
+    
     @SuppressWarnings("unchecked")
     public <T extends KService> T getService(String name) {
+    	
         Object o = serviceCache.get(name);
+        
         if (o != null) {
             return (T) o;
         }
@@ -120,28 +142,34 @@ public class KBaseServiceFactory {
             o = getService(servicePath, c);
             serviceCache.put(name, o);
         } catch (ClassNotFoundException e) {
-            logger.error(e);
+            logger.error(e.getMessage(), e);
         } catch (NoSuchFieldException e) {
-            logger.error(e);
+            logger.error(e.getMessage(), e);
         } catch (SecurityException e) {
-            logger.error(e);
+            logger.error(e.getMessage(), e);
         } catch (IllegalArgumentException e) {
-            logger.error(e);
+            logger.error(e.getMessage(), e);
         } catch (IllegalAccessException e) {
-            logger.error(e);
+            logger.error(e.getMessage(), e);
         }
+        
         return (T) o;
     }
     
+    // ----------------------------------------------------------------------
+    
     @SuppressWarnings("unchecked")
     private Class<? extends KService> getServiceClass(String name) throws ClassNotFoundException {
+    	
+    	logger.debug("getServiceClass: basePackage: " + basePackage);
+            
         if (basePackage == null) {
             basePackage = this.getClass().getPackage().getName();
         }
         
         Class<? extends KService> c = null;
         try {
-            //logger.debug("checking class for service name: " + name);
+            logger.debug("checking class for service name: " + name);
             c = (Class<? extends KService>) Class.forName(name);
         } catch (ClassNotFoundException e) {
             if (basePackage != null) {
